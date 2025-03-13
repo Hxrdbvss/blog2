c988fa099ee016acbb92660d07017a6e0188c6fa
Hxrdbvss <glebsemenov2003@gmail.com>
2025-03-13 14:50:49 +0300

    new templates

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,25 +1,20 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Category, Post
 
 def index(request):
-    posts = Post.objects.filter(is_published=True).order_by('-pub_date')
-    reversed_posts = posts[:5]  # Например, берём последние 5 постов
-    context = {'posts': reversed_posts}
-    return render(request, 'blog/index.html', context)
+    post_list = Post.objects.all()  # Получаем все посты
+    return render(request, 'blog/index.html', {'post_list': post_list})
 
 def post_detail(request, id):
-    """Страница отдельного поста."""
-    post = next((p for p in posts if p["id"] == id), None)
-    context = {"post": post}
-    if not post:
-        return render(request, '404.html', status=404)  # Если пост не найден
-    return render(request, 'blog/detail.html', context)
-    #return render(request, "blog/detail.html", context)
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'blog/detail.html', {'post': post})
 
 def category_posts(request, category_slug):
-    """Посты определенной категории."""
-    filtered_posts = [p for p in posts if p["category"] == category_slug]
-    context = {"posts": filtered_posts, "category": category_slug}
-    return render(request, "blog/category.html", context)
+    category = get_object_or_404(Category, slug=category_slug)
+    post_list = Post.objects.filter(category=category)
+    return render(request, 'blog/category.html', {
+        'category': category,
+        'post_list': post_list
+    })
 
 
--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -18,14 +18,12 @@ Including another URLconf
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from blog import views
 from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),  # Подключаем URLs для блога
-    path('pages/', include('pages.urls')),  # Подключаем URLs для страниц
-    path('', views.index, name='index'),  # Главная страница
+    path('', include('blog.urls')),
+    path('pages/', include('pages.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
