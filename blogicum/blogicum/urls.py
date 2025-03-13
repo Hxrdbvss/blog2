97f7ae2eefeb6efb4d6c98e511a9d7d3930b7a1d
Hxrdbvss <glebsemenov2003@gmail.com>
2025-03-13 14:12:44 +0300

    Fix adminka

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -19,8 +19,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from blog import views
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include('blog.urls')),  # Подключаем URLs для блога
     path('pages/', include('pages.urls')),  # Подключаем URLs для страниц
     path('', views.index, name='index'),  # Главная страница
